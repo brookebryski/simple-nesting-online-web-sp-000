20c12f5b773e6cb17cefa6b7088785867b1efba0
chic-state-0895 <brookeabryski@gmail.com>
2020-09-06 00:23:46 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -80,7 +80,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-		 
+
 		 programmer_hash[:yukihiro_matsumoto] = {
     known_for: "Ruby",
     languages:
@@ -107,7 +107,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
-		 
+
 programmer_hash[:alan_kay][:known_for] = "GUI"
 
 end
